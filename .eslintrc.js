f59cb00cc04ba5355bcd1c52c2b9b43d2bad879e
nconrad <1194246+nconrad@users.noreply.github.com>
2020-08-28 19:00:23 -0500

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'es2020': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+    'enforceDynamicLinks': 'never'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error','unix'],
+    'quotes': ['error', 'single', {'allowTemplateLiterals': true}],
+    'semi': ['error', 'never']
+  },
+  'globals': {
+    'document': true
+  }
+}
